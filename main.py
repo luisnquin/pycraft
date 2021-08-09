cc034c745f50e59ce377cbec4ac960b6022cf653
Luis Quinones <lpaandres2020@gmail.com>
2021-08-09 16:44:20 -0500

    0.5

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-from ursina import *  #
+from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
+
 grass_texture = load_texture("assets/grass_texture.png")
 dirt_texture = load_texture("assets/dirt_texture.png")
 stone_texture = load_texture("assets/stone_texture.png")
 brick_texture = load_texture("assets/brick_texture.png")
+sky_texture = load_texture("assets/skybox_texture.png")
+arm_texture = load_texture("assets/arm_texture.png")
+punch_sound = Audio("assets/punch_sound", loop=False, autoplay=False)
+
 block_pick = 1
 
 
 def update():
     global block_pick
+    if held_keys["left mouse"] or held_keys["right mouse"]:
+        hand.active()
+    else:
+        hand.passive()
 
     if held_keys["1"]:
         block_pick = 1
@@ -23,13 +32,13 @@ def update():
 
 
 class Voxel(Button):
-    def __init__(self, position=(0, 0, 0), texture=grass_texture):
+    def __init__(self, position=(0, 0, 0)):
         super().__init__(
             parent=scene,
             position=position,
             model="assets/block",
             origin_y=0.5,
-            texture=texture,
+            texture=grass_texture,
             color=color.color(0, 0, random.uniform(0.9, 1)),
             scale=0.5
         )
@@ -37,6 +46,7 @@ class Voxel(Button):
     def input(self, key):
         if self.hovered:
             if key == "right mouse down":
+                punch_sound.play()
                 if block_pick == 1:
                     voxel = Voxel(position=self.position + mouse.normal)
                     voxel.texture = grass_texture
@@ -51,13 +61,47 @@ class Voxel(Button):
                     voxel.texture = dirt_texture
 
             if key == "left mouse down":
+                punch_sound.play()
                 destroy(self)
+
+
+class Sky(Entity):
+    def __init__(self):
+        super().__init__(
+            parent=scene,
+            model="sphere",
+            texture=sky_texture,
+            scale=150,
+            double_sided=True
+        )
+
+
+class Hand(Entity):
+    def __init__(self):
+        super().__init__(
+            parent=camera.ui,
+            model="assets/arm",
+            texture=arm_texture,
+            scale=0.2,
+            rotation=Vec3(150, -10, 0),
+            position=Vec2(0.4, -0.6)
+        )
+
+    def active(self):
+        self.position = Vec2(0.3, -0.5)
+
+    def passive(self):
+        self.position = Vec2(0.4, -0.6)
 
 
 for z in range(10):
     for x in range(10):
         voxel1 = Voxel((x, 0, z))
+        voxel2 = Voxel((x, -1, z)).texture = dirt_texture
+        voxel4 = Voxel((x, -2, z)).texture = stone_texture
 
-player1 = FirstPersonController()
+player = FirstPersonController()
+sky = Sky()
+hand = Hand()
 
 app.run()
