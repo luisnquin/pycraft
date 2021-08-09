ee44a2366e6288d041260b8bc46f1de800ccece4
Luis Quinones <lpaandres2020@gmail.com>
2021-08-09 15:49:02 -0500

    0

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
+app = Ursina()
+grass_texture = load_texture("Textures/grass_texture.png")
+
 
 class Voxel(Button):
     def __init__(self, position=(0, 0, 0)):
         super().__init__(
             parent=scene,
-            model="cube",
             position=position,
+            model="textures/block",
             origin_y=0.5,
-            texture="white_cube",
-            color=color.white,
-            hightlight_color=color.lime
+            texture=grass_texture,
+            color=color.color(0, 0, random.uniform(0.9, 1)),
+            hightlight_color=color.lime,
+            scale=0.5
         )
 
+    def input(self, key):
+        if self.hovered:
+            if key == "right mouse down":
+                voxel = Voxel(position=self.position + mouse.normal)
 
-app = Ursina()
+            if key == "left mouse down":
+                destroy(self)
+
 
 for z in range(10):
     for x in range(10):
