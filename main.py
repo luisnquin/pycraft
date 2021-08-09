56af122f019a314a75bbb690a097c9283a7846d7
Luis Quinones <lpaandres2020@gmail.com>
2021-08-09 16:08:09 -0500

    0.4

--- main.py
+++ main.py
@@ -1,27 +1,54 @@
-from ursina import * #
+from ursina import *  #
 from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
-grass_texture = load_texture("Textures/grass_texture.png")
+grass_texture = load_texture("assets/grass_texture.png")
+dirt_texture = load_texture("assets/dirt_texture.png")
+stone_texture = load_texture("assets/stone_texture.png")
+brick_texture = load_texture("assets/brick_texture.png")
+block_pick = 1
+
+
+def update():
+    global block_pick
+
+    if held_keys["1"]:
+        block_pick = 1
+    if held_keys["2"]:
+        block_pick = 2
+    if held_keys["3"]:
+        block_pick = 3
+    if held_keys["4"]:
+        block_pick = 4
 
 
 class Voxel(Button):
-    def __init__(self, position=(0, 0, 0)):
+    def __init__(self, position=(0, 0, 0), texture=grass_texture):
         super().__init__(
             parent=scene,
             position=position,
-            model="textures/block",
+            model="assets/block",
             origin_y=0.5,
-            texture=grass_texture,
+            texture=texture,
             color=color.color(0, 0, random.uniform(0.9, 1)),
-            hightlight_color=color.lime,
             scale=0.5
         )
 
     def input(self, key):
         if self.hovered:
             if key == "right mouse down":
-                voxel = Voxel(position=self.position + mouse.normal)
+                if block_pick == 1:
+                    voxel = Voxel(position=self.position + mouse.normal)
+                    voxel.texture = grass_texture
+                if block_pick == 2:
+                    voxel = Voxel(position=self.position + mouse.normal)
+                    voxel.texture = stone_texture
+                if block_pick == 3:
+                    voxel = Voxel(position=self.position + mouse.normal)
+                    voxel.texture = brick_texture
+                if block_pick == 4:
+                    voxel = Voxel(position=self.position + mouse.normal)
+                    voxel.texture = dirt_texture
 
             if key == "left mouse down":
                 destroy(self)
